ed87f594f6f05f3e7fe274c6178715d335700d40
Muamer Kukic <ako@Muamers-MBP.lan>
2019-09-20 09:29:43 -0700

    hooked up

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,17 +1,45 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import { Link } from "react-router-dom";
+import SearchForm from "./SearchForm"
+const CharacterList = props =>{
+const [characters, setCharacters] = useState([])
 
-export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
-
+  
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const GetCharacters = () => {
+      axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then(response => {
+          console.log(response)
+          setCharacters(response.data.results);
+        })
+        .catch(error => {
+          console.error('Server Error', error);
+        });
+    };
+     GetCharacters();
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <SearchForm/>
+      <h2>{characters.map(character => (
+        <CharacterDetails key={character.id} character={character} />
+      ))}</h2>
     </section>
   );
 }
 
+function CharacterDetails({character}) {
+  const {name, species} = character;
+  return(
+    <div>
+      
+      <h2>{name}</h2>
+      <p>{species}</p>
+    </div>
+  )
+}
+
+export default CharacterList;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,4 @@
 import React, { useState } from "react";
 
-export default function SearchForm() {
- 
-  return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
-  );
-}
+export default function SearchForm(props) {
+  
